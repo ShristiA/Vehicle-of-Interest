85d4e194f51e301aa562d2cb0111bd4ecb566ed6
ShristiA <shristiamatya3@gmail.com>
2023-09-14 22:37:59 -0400

    all files

---
+++ VehiclesOfInterestDesktop/src/vehiclesofinterestdesktop/VehiclesOfInterestDesktop.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package vehiclesofinterestdesktop;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Michael Brown
+ */
+public class VehiclesOfInterestDesktop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //MainScreen ms  = new MainScreen();
+    
+        //System.out.println(Arrays.toString(vc.allVehicleMake()));
+    }
+    
+}
